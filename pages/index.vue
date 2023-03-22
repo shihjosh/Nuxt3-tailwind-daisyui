dd005c14c2767909d22e2956a7b7444b831e1759
josh <josh.shihtw@gmali.com>
2023-03-22 10:10:34 +0800

    基本環境建置 NUXT3+Tailwind+Daisyui

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mx-6 mb-4 bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
+      <div class="my-4 flex space-x-4">
+        <NuxtLink to="/about">前往 About</NuxtLink>
+        <NuxtLink to="/contact">前往 Contact</NuxtLink>
+      </div>
+      <div class="carousel w-full">
+  <div id="item1" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
+  </div> 
+  <div id="item2" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
+  </div> 
+  <div id="item3" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+  </div> 
+  <div id="item4" class="carousel-item w-full">
+    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
+  </div>
+</div> 
+<div class="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" class="btn btn-xs">1</a> 
+  <a href="#item2" class="btn btn-xs">2</a> 
+  <a href="#item3" class="btn btn-xs">3</a> 
+  <a href="#item4" class="btn btn-xs">4</a>
+</div>
+    </div>
+  </div>
+</template>
