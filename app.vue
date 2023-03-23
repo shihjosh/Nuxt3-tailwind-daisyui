fa3e4831ed69f4052c87effe096add14a79dd87b
josh <josh.shihtw@gmali.com>
2023-03-23 17:14:27 +0800

    修改/新增 tailwind.config.js

--- app.vue
+++ app.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="m-4 bg-white">
     <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
-    <button class="btn btn-outline btn-success">Success</button>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+    <div class="flex flex-col items-center">
+    <div class="mt-8 flex items-center">
+      <input
+        id="show-button"
+        v-model="useRound"
+        name="show-button"
+        type="checkbox"
+        class="h-5 w-5"
+      />
+      <label for="show-button" class="ml-2 block text-base text-slate-800">使用圓角按鈕</label>
+    </div>
+    <component :is="useRound ? RoundButton : BaseButton" />
+  </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const useRound = ref(false)
+const BaseButton = resolveComponent('BaseApplyButton')
+const RoundButton = resolveComponent('RoundApplyButton')
+</script>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
 module.exports = {
+    daisyui: {
+        themes: [
+          {
+            mytheme: {
+            "primary": "#d8e87d",
+            "secondary": "#bc34e5",
+            "accent": "#8fdd4b",
+            "neutral": "#1C1627",
+            "base-100": "#353347",
+            "info": "#8ED3F0",
+            "success": "#18A089",
+            "warning": "#E49A07",
+            "error": "#E2405B",
+            },
+          },
+        ],
+      },
     plugins: [require("daisyui")],
 }
